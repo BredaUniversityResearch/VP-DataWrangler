bbb2789c3a05b81b09188662dcb3549d617146e8
Phil de Groot <groot.p@buas.nl>
2022-04-22 12:54:06 +0200

    Cleared repo as the files have moved to Virtual-Productions-Template (https://github.com/BredaUniversityResearch/Virtual-Productions-Template)

--- Plugins/PhysicalObjectTracker/Source/PhysicalObjectTracker/Public/PhysicalObjectTrackingUtility.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-class PHYSICALOBJECTTRACKER_API FPhysicalObjectTrackingUtility
-{
-public:
-	static bool GetTrackedDevicePositionAndRotation(int32 SteamVRDeviceId, FVector& Position, FQuat& Orientation);
-};
-
