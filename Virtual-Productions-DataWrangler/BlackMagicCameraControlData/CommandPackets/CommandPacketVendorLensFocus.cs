08069108795ab5604d438eccf33c2d02c983aa90
Phil de Groot <groot.p@buas.nl>
2023-03-03 14:38:15 +0100

    Added command packets for focus and aperture of controllable lenses

---
+++ Virtual-Productions-DataWrangler/BlackMagicCameraControlData/CommandPackets/CommandPacketVendorLensFocus.cs
@@ -0,0 +1,32 @@
+﻿using BlackmagicCameraControlData;
+
+namespace BlackmagicCameraControl.CommandPackets
+{
+	[CommandPacketMeta(12, 11, 0, ECommandDataType.Utf8String)]
+	public class CommandPacketVendorLensFocus: ICommandPacketBase
+	{
+		public string FocusDistance;	//
+
+		public CommandPacketVendorLensFocus(CommandReader a_reader)
+		{
+			FocusDistance = a_reader.ReadString();
+		}
+
+		public override void WriteTo(CommandWriter a_writer)
+		{
+			a_writer.Write(FocusDistance);
+		}
+
+		public override bool Equals(ICommandPacketBase? a_other)
+		{
+			CommandPacketVendorLensFocus? other = (CommandPacketVendorLensFocus?)a_other;
+			return other != null &&
+			       other.FocusDistance == FocusDistance;
+		}
+
+		public override string ToString()
+		{
+			return $"{GetType().Name} [{FocusDistance}]";
+		}
+	}
+}
